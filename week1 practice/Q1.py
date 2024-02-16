682c4f3940caad263270f0c108f76790678c68d5
rahulsharmaYS <rahulsharmays97@gmail.com>
2024-02-16 12:43:44 +0530

    practicing representation learning

---
+++ week1 practice/Q1.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+#Using example datapoints to practice representation learning ~
+height = [150, 160, 170, 180, 190]
+weight = [50, 60, 70, 80, 90]
+age = [25, 30, 35, 40, 45]
+
+plt.scatter(height, weight, c=age, cmap='cool', s=100)
+
+plt.xlabel('height (cm)')
+plt.ylabel('weight (kg)')
+plt.title('height vs weight vs age')
+plt.colorbar(label='Age')
+plt.grid(True)
+
+plt.show()
