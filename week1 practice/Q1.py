4cb9f43ab1b42dfa53446d4eba1454e2f78e1870
Rahul Sharma <133980101+rahulsharmaYS@users.noreply.github.com>
2024-02-16 13:19:26 +0530

    Update Q1.py

--- week1 practice/Q1.py
+++ week1 practice/Q1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 #Using example data points to practice representation learning ~
 height = [150, 160, 170, 180, 190, 160, 160]
 weight = [50, 60, 70, 80, 90, 45, 80]
-age = [25, 30, 35, 40, 45, 42]
+age = [25, 30, 35, 40, 45, 42, 40]
 
 plt.scatter(height, weight, c=age, cmap='cool', s=100)
 
