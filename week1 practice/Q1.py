53c618109fa4f01231b305c89f673a54fb2fb40e
rahulsharmaYS <rahulsharmays97@gmail.com>
2024-02-16 16:18:32 +0530

    Practicing Representation Learning

--- week1 practice/Q1.py
+++ week1 practice/Q1.py
@@ -1,10 +1,5 @@
 import matplotlib.pyplot as plt
 
-<<<<<<< HEAD
-#Using example datapoints to practice representation learning ~
-=======
-#Using example data points to practice representation learning ~
->>>>>>> 4cb9f43ab1b42dfa53446d4eba1454e2f78e1870
 height = [150, 160, 170, 180, 190, 160, 160]
 weight = [50, 60, 70, 80, 90, 45, 80]
 age = [25, 30, 35, 40, 45, 42, 40]
